e5d06fc6aa561135d28ca8fb318c6ff0bf75458e
amos <amos.dorceus@hopitalweb.ht>
2021-05-28 16:03:18 -0400

    Apprendre a utiliser les formulaire avec angular

--- src/app/member-list/member-list.component.ts
+++ src/app/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Member } from '../models/member.model';
 import { MembersService } from '../services/members.service';
 
@@ -12,10 +13,17 @@ import { MembersService } from '../services/members.service';
 export class MemberListComponent implements OnInit {
   members : any = []
   val : any=[];
+  memberForm : FormGroup=this.fb.group({
+    firstname: new FormControl(""),
+    lastname: new FormControl(""),
+    dob: new FormControl(""),
+    sexe: new FormControl("F")
+  });
 
-  constructor(private membersService: MembersService) { }
+  constructor(private fb: FormBuilder,private membersService: MembersService) { }
 
   ngOnInit(): void {
+    
     //this.membersService.createMember(Object.assign({}, new Member("Linda", "Jacques")));
     this.membersService.getMembers().subscribe(res =>{
       this.members=[];
@@ -34,8 +42,29 @@ export class MemberListComponent implements OnInit {
 
     });
     //console.log(this.val);
-    //this.membersService.deleteMember("IXZjcViHjvraamUbWOKK");
     this.membersService.updateMember(Object.assign({}, new Member("Ruth", "Jacques")),"SR061L0LwBTIT7wJwzc1")
   }
+
+  submit(){
+  
+    let m =new Member(this.memberForm.get("firstname")?.value,this.memberForm.get("lastname")?.value,this.memberForm.get("dob")?.value,this.memberForm.get("sexe")?.value)
+    console.log(this.memberForm.get("firstname")?.value);
+    console.log(this.memberForm.get("lastname")?.value);
+    console.log(this.memberForm.get("dob")?.value);
+    console.log(this.memberForm.get("sexe")?.value);
+    this.membersService.createMember(Object.assign({}, m));
+
+
+  }
+
+  deleteMember(id:string=""){
+        console.log("testing :"+ id);
+        this.membersService.deleteMember(id);
+
+  }
+  updateMember(id:string=""){
+    console.log("testing :"+ id);
+    this.membersService.updateMember(Object.assign({}, new Member("Kinton", "Jonas")),id);
+}
 
 }
--- src/app/models/member.model.ts
+++ src/app/models/member.model.ts
@@ -3,9 +3,9 @@ export class Member {
     dob: string;
     firstname: string;
     lastname: string;
-    constructor(firstname:string, lastname: string) {
-        this.sexe ="";
-        this.dob="";
+    constructor(firstname:string, lastname: string, dob:string="",sexe:string="") {
+        this.sexe =sexe;
+        this.dob=dob;
         this.firstname=firstname;
         this.lastname =lastname;
     }
